afb38183149599a0dc6e8bc69acd9f714fc44e6b
chanchana <north1602@gmail.com>
2020-05-02 00:37:14 +0700

    Updates

---
+++ precache-manifest.98a664c8c7aea46901a62162554eedd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0f5b9c97379c4c6f605a2dcb9e6b8f",
+    "url": "/react-dashboard/index.html"
+  },
+  {
+    "revision": "21d29d78ced7e2bbef27",
+    "url": "/react-dashboard/static/css/2.d0625acf.chunk.css"
+  },
+  {
+    "revision": "e13577f11fbdfcf49ead",
+    "url": "/react-dashboard/static/css/main.a5b8e880.chunk.css"
+  },
+  {
+    "revision": "21d29d78ced7e2bbef27",
+    "url": "/react-dashboard/static/js/2.fb99d249.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/react-dashboard/static/js/2.fb99d249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13577f11fbdfcf49ead",
+    "url": "/react-dashboard/static/js/main.9e012fbd.chunk.js"
+  },
+  {
+    "revision": "6441731160d1bacce22c",
+    "url": "/react-dashboard/static/js/runtime-main.e109b827.js"
+  },
+  {
+    "revision": "a489bbb88f8fa8add3a361d1d077b438",
+    "url": "/react-dashboard/static/media/icon-up.a489bbb8.gif"
+  },
+  {
+    "revision": "36f7d86ea928928e05bd50c1e758eb71",
+    "url": "/react-dashboard/static/media/simple-map.36f7d86e.png"
+  }
+]);
